0412eb67b80d1a8261237527d369f04cdbef12e6
Victor Caruso <vic.caruso@gmail.com>
2022-04-27 16:38:45 -0700

    Test to render a list of pokemon and filter down to one

---
+++ src/views/Pokemon/PokemonList.test.js
@@ -0,0 +1,28 @@
+;import { render, screen, waitFor } from '@testing-library/react';
+import { capitalizeFirst } from '../../utils/capitalize';
+import userEvent from '@testing-library/user-event';
+import PokemonList from './PokemonList';
+
+describe('PokemonList', () => {
+  it('should render a list of pokemon cards to screen and then filter down to one', async () => {
+    
+    render(
+      <PokemonList />
+    )
+
+    await screen.findByText('Geodude');
+
+    const filter = screen.getByPlaceholderText('Filter pokemon');
+
+    userEvent.type(filter, 'arb');
+
+    screen.debug();
+
+    return waitFor(() => {
+      const result = screen.getAllByRole('heading');
+      expect(result.length).toEqual(1);
+      expect(result[0].textContent).toEqual('Arbok')
+    })
+
+  });
+});
