819e806df6231ae8af4c56ae814b2e2cfbb9b765
Victor Caruso <vic.caruso@gmail.com>
2022-04-27 15:57:16 -0700

    Add ability to filter pokemon

--- src/views/Pokemon/PokemonList.jsx
+++ src/views/Pokemon/PokemonList.jsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from 'react';
 import { fetchAllPokemon } from '../../services/api';
 import PokemonCard from '../../components/Pokemon/PokemonCard';
+import Search from '../../components/Search/Search';
 
 export default function PokemonList() {
   const [pokemons, setPokemons] = useState([]);
+  const [filteredPokes, setFilteredPokes] = useState([]);
+
+  const handleSearch = (search) => {
+    const filteredPokemon = pokemons.filter(pokemon => pokemon.pokemon.includes(search));
+    console.log(filteredPokemon);
+    setFilteredPokes(filteredPokemon);
+  }
 
   useEffect(() => {
    async function fetch() {
@@ -16,7 +24,12 @@ export default function PokemonList() {
   
   return (
     <div className='pokemon-list'>
-      { pokemons.map(pokemon => <PokemonCard key={pokemon._id} pokemon={pokemon} />) } 
+      <Search onSearch={handleSearch} />
+      { 
+        filteredPokes.length
+        ? filteredPokes.map(pokemon => <PokemonCard key={pokemon._id} pokemon={pokemon} />)
+        : pokemons.map(pokemon => <PokemonCard key={pokemon._id} pokemon={pokemon} />) 
+      } 
     </div>
   );
 };
